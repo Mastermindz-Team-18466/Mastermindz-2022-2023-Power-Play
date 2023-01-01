8fac7d5638cc2e30a7a9993837e06be5c94d9e2b
Aadi <61665590+AadiDahake@users.noreply.github.com>
2022-12-31 16:21:59 -0800

    update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testClasses/analogPortTest.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.testClasses;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
+
+@TeleOp
+public class analogPortTest extends LinearOpMode {
+
+    private AnalogInput analogInput;
+    private DcMotor turretMotor;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        turretMotor = hardwareMap.get(DcMotor.class, "turret");
+        analogInput = hardwareMap.get(AnalogInput.class, "analog_input");
+        waitForStart();
+        while (opModeIsActive()) {
+            if (gamepad1.a){
+                turretMotor.setPower(0.4);
+            } else if (gamepad1.b){
+                turretMotor.setPower(-0.4);
+            } else{
+                turretMotor.setPower(0);
+            }
+            telemetry.addData("TurretVoltage", analogInput.getVoltage());
+            telemetry.update();
+        }
+    }
+}
