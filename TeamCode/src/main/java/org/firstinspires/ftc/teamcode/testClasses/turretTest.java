c266627427c9b7a7e30a411cd0c5d12738ff44d7
Lavi Avigad <92415264+lavigad1@users.noreply.github.com>
2023-01-08 22:54:49 -0800

    turret pidf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testClasses/turretTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testClasses/turretTest.java
@@ -7,15 +7,30 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 @TeleOp
 public class turretTest extends LinearOpMode {
 
+    private static final double Kp = 0.01;
+    private static final double Ki = 0.005;
+    private static final double Kd = 0.001;
+    private static final double Kf = 0.00;
+
     private DcMotorEx turretMotor;
 
     @Override
     public void runOpMode() throws InterruptedException {
         turretMotor = hardwareMap.get(DcMotorEx.class, "turretMotor");
+        double integral = 0;
+        double previousError = 0;
 
         waitForStart();
         while (opModeIsActive()) {
-            turretMotor.setPower(gamepad1.left_stick_y);
+            double targetPosition = gamepad2.left_stick_y;
+            double error  = targetPosition - turretMotor.getCurrentPosition();
+            integral = integral + error;
+            double derivative = error - previousError;
+            previousError = error;
+
+            double power = Kp * error + Ki * integral + Kd * derivative + Kf;
+
+            turretMotor.setPower(power);
         }
     }
 }
