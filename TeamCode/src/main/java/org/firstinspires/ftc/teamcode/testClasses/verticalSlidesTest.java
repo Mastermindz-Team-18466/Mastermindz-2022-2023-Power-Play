ae2d6746f410bfc5443ec69e484dc44dfa368d14
Lavi Avigad <92415264+lavigad1@users.noreply.github.com>
2023-01-08 21:25:04 -0800

    elevator feedforward

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testClasses/verticalSlidesTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testClasses/verticalSlidesTest.java
@@ -6,6 +6,7 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.controller.PIDFController;
+import com.arcrobotics.ftclib.controller.wpilibcontroller.ElevatorFeedforward;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -22,6 +23,15 @@ public class verticalSlidesTest extends OpMode {
     private DcMotorEx liftMotor1;
     private DcMotorEx liftMotor2;
 
+    private static final double kS = 0.1; // position error gain
+    private static final double kG = 0.05; // velocity error gain
+    private static final double kA = 0.01; // acceleration error gain
+    private static final double kV = 0.1; // jerk error gain
+    private ElevatorFeedforward feedforward;
+    private static final double maxForce = 769/740;
+    double negativeMaxForce = -maxForce;
+
+
     @Override
     public void init() {
         controller = new PIDFController(kp, ki, kd, f);
@@ -32,6 +42,8 @@ public class verticalSlidesTest extends OpMode {
         liftMotor2 = hardwareMap.get(DcMotorEx.class, "liftMotor2");
 
         liftMotor2.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        feedforward = new ElevatorFeedforward(kS, kG, kA, kV);
     }
 
     @Override
@@ -46,6 +58,11 @@ public class verticalSlidesTest extends OpMode {
 
         liftMotor1.setPower(power1);
         liftMotor2.setPower(power1);
+
+        double feedforwardForce = feedforward.calculate(liftMotor2.getVelocity());
+        feedforwardForce = Math.max(-maxForce, Math.min(maxForce, feedforwardForce));
+        liftMotor1.setPower(feedforwardForce);
+        liftMotor2.setPower(feedforwardForce);
 
         telemetry.addData("pos1", pos1);
         telemetry.addData("target", target);
